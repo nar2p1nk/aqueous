10e963416a265f60810097868ebd693805fbd0b8
nar2p1nk <farrisisqandar@gmail.com>
2022-08-12 02:03:39 +0800

    added a description

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
     <div className="App">
       <div className='main' style={results ? {display:'none'} : {}}>
       <span className='title'>Welcome to aqueous!</span>
+        <p className="description">Get your current weather forecast now!</p>
       </div>
       <div className='weather-component' style={results ? {} : {display:'none'}}>
        <ReactWeather
