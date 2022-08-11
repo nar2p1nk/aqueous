fc44ba386fc54b92fbdc7fcddaa2de7785806a35
nar2p1nk <farrisisqandar@gmail.com>
2022-08-11 14:34:44 +0800

    weather component can now turn invisible

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
+import ReactWeather,{useOpenWeather} from 'react-open-weather';
 import './App.css';
+
 function App() {
+
+
+  const [getCountry,setGetCountry] = useState('')
+
+  const [visible,setVisible] = useState(true)
+
+  const {data,isLoading,errorMessage} = useOpenWeather({
+    key:'09837498dea54af38d04ed82007c8cdc',
+    lat:2.5,
+    lon:112.5,
+    lang:'en',
+    unit:'metric'
+  })
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className='weather-component' style={visible ? {visibility:'hidden'} : {}}>
+       <ReactWeather
+          isLoading={isLoading}
+          errorMessage={errorMessage}
+          data={data}
+          lang="en"
+          locationLabel="Malaysia"
+          unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+          showForecast
+      />
+      </div>
     </div>
   );
 }
